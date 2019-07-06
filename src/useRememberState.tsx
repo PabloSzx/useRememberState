8ee0501d113b045efb7605eb9e77b2b0d8831a8f
PabloSzx <pablosaez1995@gmail.com>
2019-07-06 18:00:02 -0400

    improve usestate usage

--- src/useRememberState.tsx
+++ src/useRememberState.tsx
@@ -2,30 +2,28 @@ import { useState, useEffect } from 'react';
 
 const checkLocalStorage = () => typeof localStorage !== undefined;
 
+function getLocalStorage<T = string>(name: string, defaultV: T) {
+  if (checkLocalStorage()) {
+    const v = localStorage.getItem(name);
+    if (v) return JSON.parse(v) as T;
+  }
+  return defaultV;
+}
+function setLocalStorage<T = string>(v: T, name: string) {
+  if (checkLocalStorage()) localStorage.setItem(name, JSON.stringify(v));
+}
+
 function useRememberState<T = string>(
   consistentName: string,
   defaultValue: T
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [state, setState] = useState<T>(defaultValue);
+  const [state, setState] = useState<T>(() =>
+    getLocalStorage<T>(consistentName, defaultValue)
+  );
 
   useEffect(() => {
-    setState(getLocalStorage(consistentName, defaultValue));
-  }, [consistentName, defaultValue]);
-
-  useEffect(() => {
-    setLocalStorage(state, consistentName);
-  }, [state, consistentName]);
-
-  const getLocalStorage = (name: string, defaultV: T) => {
-    if (checkLocalStorage()) {
-      const v = localStorage.getItem(name);
-      if (v) return JSON.parse(v) as T;
-    }
-    return defaultV;
-  };
-  const setLocalStorage = (v: T, name: string) => {
-    if (checkLocalStorage()) localStorage.setItem(name, JSON.stringify(v));
-  };
+    setLocalStorage<T>(state, consistentName);
+  }, [state]);
 
   return [state, setState];
 }
